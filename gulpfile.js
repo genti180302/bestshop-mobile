bc6e54d455c5e5f3fcde0285c518274661e3cb56
genti180302 <gentian_1@live.at>
2024-07-26 16:09:09 +0200

    Finished bestshop

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require ('gulp');
+const sass = require ('gulp-sass')(require("sass"));
+
+const entryPath = 'Best;
+
+gulp.task('compileSass', function () {
+    return gulp.src(entryPath + "/scss/main.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(entryPath + "/css"));
+});
+
+
+gulp.task("default", gulp.series("compileSass"))
