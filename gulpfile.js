05c6cddc866e076fb5e8a22e9a5025b94443f7f4
genti180302 <gentian_1@live.at>
2024-07-31 07:14:43 +0200

    deploy fixes

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,11 @@ gulp.task('build', gulp.series('compileSass'));
 gulp.task('default', gulp.series('build')); */
 
 
-const gulp = require('gulp');
+
+
+
+
+/* const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css'); // Optional: For minifying CSS
 const del = require('del');
@@ -61,5 +65,42 @@ gulp.task('copy-html', function () {
 gulp.task('build', gulp.series('clean', 'compileSass', 'copy-html'));
 
 // Default task
+gulp.task('default', gulp.series('build')); */
+
+
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+
+// Set paths
+const entryPath = 'BestShopMobile';
+const outputPath = 'dist';
+
+// Clean the dist directory before building
+gulp.task('clean', async function () {
+    const del = await import('del');
+    return del.deleteAsync([outputPath]); // Use del.deleteAsync() for the async function in ES module version
+});
+
+// Compile SASS to CSS, optionally minify, and output to dist
+gulp.task('compileSass', function () {
+    return gulp.src(entryPath + '/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS()) // Optional: Remove if you don't want to minify CSS
+        .pipe(gulp.dest(outputPath + '/css'));
+});
+
+// Copy HTML files to dist
+gulp.task('copy-html', function () {
+    return gulp.src(entryPath + '/*.html')
+        .pipe(gulp.dest(outputPath));
+});
+
+// Build task
+gulp.task('build', gulp.series('clean', 'compileSass', 'copy-html'));
+
+// Default task
 gulp.task('default', gulp.series('build'));
+
 
